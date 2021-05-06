f6b01b0d4db53d9e1901c7bb2bf2c66fba8f6a64
Chloe Everson <chloeliz93@gmail.com>
2021-05-06 11:14:24 -0500

    set up return for customer component

--- src/components/Customer/Customer.jsx
+++ src/components/Customer/Customer.jsx
@@ -4,7 +4,31 @@ function Customer(){
     return(
         <>
             <h2>Step 2: Customer Information</h2>
-            <input placeholder=
+            <form onSubmit={handleSubmit} className="add-customer-form">
+                <input placeholder="Name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                />
+
+                <input placeholder="Street Address"
+                value={address}
+                onChange={(event) => setAddress(event.target.value)}
+                />
+
+                <input placeholder="City"
+                value={city}
+                onChange={(event) => setCity(event.target.value)}
+                />
+
+                <input placeholder="Zip"
+                value={zip}
+                onChange={(event) => setZip(event.target.value)}
+                />
+
+                <button>NEXT</button>
+
+
+            </form>
         </>
     )
 }
