f32f1bad669ad74f7dcae7ca62c1ff8696380747
Charlie Stokes <chstokes7@gmail.com>
2021-05-06 10:42:55 -0500

    basic router and index store set up

--- src/index.js
+++ src/index.js
@@ -3,4 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const menuStore = (state = [], action) => {
+
+    if (action.type === 'SET_MENU') {
+        return action.payload;
+    }
+}
+
+const storeInstance = createStore(
+    // reducers go inside our store - these are specific to our app
+    combineReducers({
+        menuStore,
+
+    }
+    ),
+    // Also, add our middleware for the logger
+    applyMiddleware(
+        logger
+    )
+
+);
+
+ReactDOM.render(
+<React.StrictMode>
+        <Provider store={storeInstance}>
+            <App />
+        </Provider>
+    </React.StrictMode>, 
+document.getElementById('root'));
